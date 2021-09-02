4e773537acfefea724b11dc9aec9404eea661964
DownOnTheFarm <81724380+DownOnTheFarm@users.noreply.github.com>
2021-09-01 21:05:12 -0700

    I am tired

--- charging_map.js
+++ charging_map.js
@@ -2,10 +2,53 @@
 
 var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&state=CA&limit=all"
 
+var myMap = L.map("map", {
+    center: [36.44, -119.46],
+    zoom: 6.4
+  });
+
+function createMap(chargers) {
+    
+    
+    
+     // Add a tile layer.
+     var charge = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+}
+
+
 // Perform a GET request to the query URL/
-d3.json(queryURL).then(function (data) {
+d3.json(queryURL).then(function (response) {
     // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.fuel_stations);
+    // Create a new marker cluster group.
+     
+    var markers = L.marker([
+        L.latLng(
+            parseFloat(item['latitude']),
+            parseFloat(item['longitude'])
+        )
+    ]);
+
+    // Loop through the data.
+    for (var i = 0; i < response.length; i++) {
+  
+      // Set the data location property to a variable.
+      var location = response[i].fuel_stations;
+  
+      // Check for the location property.
+      if (location) {
+  
+        // Add a new marker to the cluster group, and bind a popup.
+        markers.addLayer(L.marker([fuel_stations.latitude, fuel_stations.longitude])
+          .bindPopup(response[i].city));
+      }
+
+  }
+
+  // Add our marker cluster layer to the map.
+  myMap.addLayer(markers);
+
 });
 
 function createFeatures(chargingData) {
@@ -18,17 +61,6 @@ function createFeatures(chargingData) {
     });
 
     createMap(chargers);
-}
+};
 
-function createMap(chargers) {
-    
-    var myMap = L.map("map", {
-        center: [36.44, -119.46],
-        zoom: 6.4
-      });
-    
-     // Add a tile layer.
-     var charge = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
-}
+console.log(myMap)
