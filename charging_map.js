6ea38c197f00b487b277a8a1f08874731c15c7f8
DownOnTheFarm <81724380+DownOnTheFarm@users.noreply.github.com>
2021-09-08 19:46:20 -0700

    Pushing what I have

--- charging_map.js
+++ charging_map.js
@@ -2,58 +2,50 @@
 
 var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&state=CA&limit=all"
 
-var myMap = L.map("map", {
-    center: [36.44, -119.46],
-    zoom: 6.4
-  });
-
-function createMap(chargers) {
-    
-     // Add a tile layer.
-     var charge = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
-}
-
-
 // Perform a GET request to the query URL/
-d3.json(queryURL).then(function (response) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    // Create a new marker cluster group.
-     
-    var markers = L.marker();
-
-    // Loop through the data.
-    for (var i = 0; i < response.length; i++) {
-  
-      // Set the data location property to a variable.
-      var location = response[i].fuel_stations;
-  
-      // Check for the location property.
-      if (location) {
-  
-        // Add a new marker to the cluster group, and bind a popup. ***THIS IS WHERE I AM STUCK!***
-        markers.addLayer(L.marker(parseFloat(markers[fuel_stations.latitude]), parseFloat(markers[fuel_stations.longitude]))
-          .bindPopup(response[i].city));
-      }
-
-  }
-
-  // Add our marker cluster layer to the map.
-  myMap.addLayer(markers);
-
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
 });
 
 function createFeatures(chargingData) {
-    function onEachFeature(feature, layer) {
-        layer.bindPopup('<h3>${feature.fuel_stations.city}</h3>');
-    }
 
-    var chargers = L.geoJSON(chargingData, {
-        onEachFeature : onEachFeature
-    });
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(feature.fuel_stations.street_address)
+  }
 
-    createMap(chargers);
+  var chargeStations = L.geoJSON(chargingData, {
+    onEachFeature: onEachFeature
+  })
 };
 
-console.log(myMap)
+
+function createMap(chargeStations) {
+
+  // Create the tile layer that will be the background of our map.
+  var chargeMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+  
+  // Create a baseMaps object to hold the chargeMap layer.
+  var baseMaps = {
+    "Charger Map": chargeMap
+  };
+  // Create an overlayMaps object to hold the chargeStations layer.
+  var overlayMaps = {
+    "Charging Stations": chargeStations
+  };
+
+  var myMap = L.map("map", {
+    center: [36.44, -119.46],
+    zoom: 6.4,
+    layers: [chargeMap,chargeStations]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap)
+
+  console.log(myMap)
+};
+
