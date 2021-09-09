edc9bf11ef578042baaed4ba556775e426d39cc9
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-08 19:57:39 -0700

    Update Nest

--- ev_population.js
+++ ev_population.js
@@ -10,22 +10,22 @@ var yearlyData = d3.csv(csvdata).then(function(data) {
 
 
 // Attempt to nest and group by year. Will also need to pull out by fuel type eventually, but one step at a time.
-var groupedData = d3.nest()
-.key(function(d) {return d.novehicles; })
-.rollup(function(v) {return d3.sum(v, function(d) {return d.novehicles; }); })
-.object(yearlyData);
-console.log(groupedData);
+// var groupedData = d3.nest()
+// .key(function(d) {return d.novehicles; })
+// .rollup(function(v) {return d3.sum(v, function(d) {return d.novehicles; }); })
+// .object(yearlyData);
+// console.log(groupedData);
 
 
 var groupedData = d3.nest()
-.key(function(d) {return d.novehicles; })
+.key(function(d) {return d.datayear; })
 .rollup(function(v) {
   return {
-    d3.sum(v, function(d) {return d.novehicles; }); })
-.object(yearlyData);
-console.log(groupedData);
-
-  } 
+    novehicles: d3.sum(d3.values(v[0].novehicles[0]))
+  }
+})
+.map(data);
+console.log(JSON.stringify(groupedData));
 
 });
 
