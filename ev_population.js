a06fde90285e3d0d99a4861e34dfbf16edf6f5a0
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-08 19:09:43 -0700

    Grouping

--- ev_population.js
+++ ev_population.js
@@ -1,3 +1,18 @@
-d3.csv("/Data/Vehicle_Pop_County_04-30-2021.csv").then(function(data) {
-  console.log(data[0]);
+// Read in csv from git
+const csvdata = "https://raw.githubusercontent.com/sissa81/legendary-system/choxie1/Data/Zip_Pop_04-30-2021.csv";
+
+// Fetch csv data and console log it
+
+var yearlydata = d3.csv(csvdata).then(function(data) {
+  console.log(data);
+
+// Attempt to nest and group by year. Will also need to pull out by fuel type eventually, but one step at a time.
+var groupedData = d3.nest()
+.key(function(d) {return d.novehicles; })
+.rollup(function(v) {return d3.sum(v, function(d) {return d.amount; }); })
+.object(yearlydata);
+console.log(groupedData);
+
 });
+
+
