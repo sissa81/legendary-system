8197524e750dad10074576897a1285565214ed08
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-08 20:31:17 -0700

    Sum and Nested

--- ev_population.js
+++ ev_population.js
@@ -10,22 +10,38 @@ var yearlyData = d3.csv(csvdata).then(function(data) {
 
 
 // Attempt to nest and group by year. Will also need to pull out by fuel type eventually, but one step at a time.
-// var groupedData = d3.nest()
-// .key(function(d) {return d.novehicles; })
-// .rollup(function(v) {return d3.sum(v, function(d) {return d.novehicles; }); })
-// .object(yearlyData);
-// console.log(groupedData);
-
 
 var groupedData = d3.nest()
 .key(function(d) {return d.datayear; })
-.rollup(function(v) {
-  return {
-    novehicles: d3.sum(d3.values(v[0].novehicles[0]))
-  }
-})
+.rollup(function(v) {return d3.sum(v, function(d) {return d.novehicles; }); })
 .map(data);
-console.log(JSON.stringify(groupedData));
+console.log(groupedData);
+
+
+// var groupedData = d3.nest()
+// .key(function(d) {return d.datayear; })
+// .rollup(function(v) {
+//   return {
+//     novehicles: d3.sum(d3.values(v[0].novehicles[0]))
+//   }
+// })
+// .map(data);
+// console.log(JSON.stringify(groupedData));
+
+
+// var groupedData = d3.nest()
+// .key(function(d) {return (d.datayear) })
+// .rollup(function(v) {
+//   return {
+//     novehicles: d3.sum(v, function(d) {
+//       var type = d3.values(d.novehicles);
+//       var array = type.map(function (x) {return parseInt(x, 10) });
+//       return d3.sum(array);
+//     })
+//   }
+// })
+// .entries(data);
+// console.log(JSON.stringify(groupedData));
 
 });
 
