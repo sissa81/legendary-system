df7528c6e0d8fee9fa62f8f4ff094b2ee7431a2d
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-08 19:49:04 -0700

    Update from String to Number

--- ev_population.js
+++ ev_population.js
@@ -2,16 +2,30 @@
 const csvdata = "https://raw.githubusercontent.com/sissa81/legendary-system/choxie1/Data/Zip_Pop_04-30-2021.csv";
 
 // Fetch csv data and console log it
-
-var yearlydata = d3.csv(csvdata).then(function(data) {
+// Convert # of vehicles to number instead of string
+var yearlyData = d3.csv(csvdata).then(function(data) {
+  data.forEach(function(d){d['novehicles'] = +d['novehicles']; });
   console.log(data);
+
+
 
 // Attempt to nest and group by year. Will also need to pull out by fuel type eventually, but one step at a time.
 var groupedData = d3.nest()
 .key(function(d) {return d.novehicles; })
-.rollup(function(v) {return d3.sum(v, function(d) {return d.amount; }); })
-.object(yearlydata);
+.rollup(function(v) {return d3.sum(v, function(d) {return d.novehicles; }); })
+.object(yearlyData);
 console.log(groupedData);
+
+
+var groupedData = d3.nest()
+.key(function(d) {return d.novehicles; })
+.rollup(function(v) {
+  return {
+    d3.sum(v, function(d) {return d.novehicles; }); })
+.object(yearlyData);
+console.log(groupedData);
+
+  } 
 
 });
 
