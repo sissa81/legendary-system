9159ccfc6fba4d2640f5b18187f2f4076a5fd3fb
phenomenon1223 <phenomenon1223@gmail.com>
2021-09-11 11:08:20 -0700

    updated codes

--- Part III/maps/logic.js
+++ Part III/maps/logic.js
@@ -1,55 +1,63 @@
-function createMap(fuel_stations) {
+var myMap = L.map("map-id", {
+  center: [36.44, -119.46],
+  zoom: 6.4,
+  // layers: [chargeMap, fuel_stations]
+});
 
-  var chargeMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+}).addTo(myMap);
 
-  var baseMaps = {
-    "Charge Map": chargeMap
-  };
+var url = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=all"
 
-  var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=all"
+d3.json(url).then(function(response) {
+  console.log(response);
+  
+  var heatArray = [];
 
-  d3.json(queryURL).then(function (data) {    
-   for (var = 0; i < data.length; i++) {
-     var fuel_stations = data[i].fuel_stations;
-     if (fuel_stations) {
-       L.marker([fuel_stations.latitude, fuel_stations.longitude]).addTo(myMap);
-     }
-   }
-  });
-
-  var overlayMaps = {
-    "Supercharger Stations": fuel_stations
-  };
-
-  var map = L.map("map-id", {
-    center: [36.44, -119.46],
-    zoom: 6.4,
-    layers: [chargeMap, fuel_stations]
-  });
-
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-}
-
-function createMarkers(response) {
-
-  var stations = response.data.stations;
-
-  var fuelMarkers = [];
-
-  for (var index = 0; index < stations.length; index++) {
-    var station = stations[index];
-
-    var fuelMarker = L.marker([station.lat, station.lon])
-      .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
-    fuelMarkers.push(fuelMarker);
+  for (var i = 0; i < response.length; i++) {
+    var fuel_stations = data[i].fuel_stations;
+    if (fuel_stations) {
+      heatArray.push([fuel_stations.latitude, fuel_stations.longitude]);
+    }
   }
+  
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(myMap);
 
-  createMap(L.layerGroup(fuelMarkers));
-}
+});
 
-d3.json(queryURL).then(createMarkers);
+//   var baseMaps = {
+//     "Charge Map": chargeMap
+//   };
+
+//   var overlayMaps = {
+//     "Supercharger Stations": fuel_stations
+//   };
+
+//   L.control.layers(baseMaps, overlayMaps, {
+//     collapsed: false
+//   }).addTo(map);
+// }
+
+// function createMarkers(response) {
+
+//   var stations = response.data.stations;
+
+//   var fuelMarkers = [];
+
+//   for (var index = 0; index < stations.length; index++) {
+//     var station = stations[index];
+
+//     var fuelMarker = L.marker([station.lat, station.lon])
+//       .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+
+//     fuelMarkers.push(fuelMarker);
+//   }
+
+//   createMap(L.layerGroup(fuelMarkers));
+// }
+
+// d3.json(queryURL).then(createMarkers);
