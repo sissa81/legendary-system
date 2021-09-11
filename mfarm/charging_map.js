865e35d6c5b571f528c2e6da5ab820fa3fdb8856
DownOnTheFarm <81724380+DownOnTheFarm@users.noreply.github.com>
2021-09-11 09:59:35 -0700

    Markers and MORE

--- mfarm/charging_map.js
+++ mfarm/charging_map.js
@@ -1,62 +1,66 @@
-// Store Charging API as queryURL
-
-
-
-//function createFeatures(chargingData) {
-
- // function onEachFeature(feature, layer) {
- //   layer.bindPopup(feature.fuel_stations.street_address)
- // }
-
- // var chargeStations = L.geoJSON(chargingData, {
- //   onEachFeature: onEachFeature
-//  })
-//};
-
-
 // Create Map
-var myMap = L.map("map", {
-  center: [36.44, -119.46],
-  zoom: 6.4,
-});
+function createMap(charging) {
+  
+  var chargeMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  var myMap = L.map("map", {
+    center: [36.44, -119.46],
+    zoom: 6.4,
+    layers: [chargeMap, charging]
+  });
+}
+
+
 
 // Create the tile layer that will be the background of our map.
-var chargeMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
-console.log(myMap)
+
+//console.log(myMap)
 // Create the markers for chargeStations
 
 var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=all"
 
 // Perform a GET request to the query URL/
 d3.json(queryURL).then(function(response) {
-  console.log(response)
- for (var i = 0; i < response.length; i++) {
+//  console.log(response)
+  var chargeMarkers = L.markerClusterGroup();
 
-   var fuel_stations = response[i].fuel_stations;
-    
+  for (var i = 0; i < response.length; i++) {
+
+    var fuel_stations = response[i].fuel_stations;
+//    console.log(fuel_stations)
    if (fuel_stations) {
-     L.marker([fuel_stations.latitude, fuel_stations.longitude]).addTo(myMap);
+      chargeMarkers.addLayer(L.marker([fuel_stations.latitude, fuel_stations.longitude]).bindPopup(fuel_stations.station_name));
    }   
  }
+
+ myMap.addLayer(chargeMarkers);
 });
 
-  // Create an overlayMaps object to hold the chargeStations layer.
- // var overlayMaps = {
- //   "Charging Stations": chargeStations
-  //};
+//function createMarkers(response) {
 
-//  var myMap = L.map("map", {
- //   center: [36.44, -119.46],
- //   zoom: 6.4,
- //   layers: [chargeMap,chargeStations]
- // });
+//  var chargeStations = response.fuel_stations;
 
- // L.control.layers(baseMaps, overlayMaps, {
-  //  collapsed: false
-//  }).addTo(myMap)
+  //var chargeMarkers = L.markerClusterGroup();
 
-//  console.log(myMap)
-//};
+  //for (var index = 0; index < chargeStations.length; index++) {
+    //var chargeStation = chargeStations[index].fuel_stations;
+
+   // if (chargeStation) {
+     // chargeMarkers.addLayer(L.marker([chargeStation.latitude, chargeStation.longitude]))
+       // .bindPopup("<h3>" + chargeStation.station_name + "</h3>");
+    //}
+    //console.log(chargeStation.longitude)
+    //var chargeMarker = L.marker([chargeStation.latitude, chargeStation.longitude])
+    //  .bindPopup("<h3>" + chargeStation.station_name + "</h3>");
+    
+    //chargeMarkers.push(chargeMarker);
+  //}
+  
+  //createMap(L.layerGroup(chargeMarkers));
+  //console.log(chargeMarkers)
+//}
+
+//d3.json(queryURL).then(createMarkers);
