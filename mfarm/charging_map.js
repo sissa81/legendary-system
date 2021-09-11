4762eeb27bedab5c9d7ed6dfe079b0d9369132cc
DownOnTheFarm <81724380+DownOnTheFarm@users.noreply.github.com>
2021-09-11 12:02:06 -0700

    markers

--- mfarm/charging_map.js
+++ mfarm/charging_map.js
@@ -10,57 +10,30 @@ function createMap(charging) {
     zoom: 6.4,
     layers: [chargeMap, charging]
   });
-}
+};
 
-
-
-// Create the tile layer that will be the background of our map.
-
-
-//console.log(myMap)
 // Create the markers for chargeStations
 
-var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=all"
+var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=10000"
 
-// Perform a GET request to the query URL/
-d3.json(queryURL).then(function(response) {
-//  console.log(response)
-  var chargeMarkers = L.markerClusterGroup();
-
-  for (var i = 0; i < response.length; i++) {
-
-    var fuel_stations = response[i].fuel_stations;
-//    console.log(fuel_stations)
-   if (fuel_stations) {
-      chargeMarkers.addLayer(L.marker([fuel_stations.latitude, fuel_stations.longitude]).bindPopup(fuel_stations.station_name));
-   }   
- }
-
- myMap.addLayer(chargeMarkers);
-});
-
-//function createMarkers(response) {
-
-//  var chargeStations = response.fuel_stations;
-
-  //var chargeMarkers = L.markerClusterGroup();
-
-  //for (var index = 0; index < chargeStations.length; index++) {
-    //var chargeStation = chargeStations[index].fuel_stations;
-
-   // if (chargeStation) {
-     // chargeMarkers.addLayer(L.marker([chargeStation.latitude, chargeStation.longitude]))
-       // .bindPopup("<h3>" + chargeStation.station_name + "</h3>");
-    //}
-    //console.log(chargeStation.longitude)
-    //var chargeMarker = L.marker([chargeStation.latitude, chargeStation.longitude])
-    //  .bindPopup("<h3>" + chargeStation.station_name + "</h3>");
-    
-    //chargeMarkers.push(chargeMarker);
-  //}
-  
-  //createMap(L.layerGroup(chargeMarkers));
-  //console.log(chargeMarkers)
-//}
-
-//d3.json(queryURL).then(createMarkers);
+// Perform a GET request to the query URL
+function createMarkers(response) {
+  console.log(response)
+  // Pull down to the fuel_stations level for information
+  var chargeStations = response.fuel_stations;
+  console.log(chargeStations)
+  // Initialize array for Charger Markers
+  var chargeMarkers = [];
+  //Loop through the Stations array
+  for (var i = 0; i < chargeStations.length; i++) {
+    var charger = chargeStations[i];
+    // For each station, create marker, and bind a pop-up
+    var chargeMarker = L.marker([charger.latitude, charger.longitude])
+      .bindPopup("<h3>" + charger.station_name + "<h3/>");
+    // Add marker to the chargeMarkers array
+    chargeMarkers.push(chargeMarker);
+  }
+  // Create a layer group from Markers and pass to createMap
+  createMap(L.layerGroup(chargeMarkers));
+};
+d3.json(queryURL).then(createMarkers);
