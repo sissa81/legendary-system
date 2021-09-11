3caac093f2a09aa4fc33754f9cd71e051016f1ce
DownOnTheFarm <81724380+DownOnTheFarm@users.noreply.github.com>
2021-09-11 14:04:56 -0700

    Marker edits

--- mfarm/charging_map.js
+++ mfarm/charging_map.js
@@ -14,7 +14,8 @@ function createMap(charging) {
 
 // Create the markers for chargeStations
 
-var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=public&state=CA&limit=10000"
+var queryURL = 
+    "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=1YJXSdZ3NnAyyfQjUFBbTnYFAF9lp57gm2pKe94r&fuel_type=ELEC&status=E&access=all&state=CA&limit=200"
 
 // Perform a GET request to the query URL
 function createMarkers(response) {
@@ -24,12 +25,18 @@ function createMarkers(response) {
   console.log(chargeStations)
   // Initialize array for Charger Markers
   var chargeMarkers = [];
+  // Set icon style
+  var myIcon = L.icon({
+    iconUrl: 'markers/lightning.png',
+    shadowUrl: 'markers/markers-shadow.png'
+  });
   //Loop through the Stations array
   for (var i = 0; i < chargeStations.length; i++) {
     var charger = chargeStations[i];
     // For each station, create marker, and bind a pop-up
-    var chargeMarker = L.marker([charger.latitude, charger.longitude])
-      .bindPopup("<h3>" + charger.station_name + "<h3/>");
+    var chargeMarker = L.marker([charger.latitude, charger.longitude], {icon: myIcon})
+      .bindPopup("<h4>" + charger.station_name + "<h/4><br><h5>" + charger.street_address + 
+        "<br> " + charger.city + ", " + charger.state + " " + charger.zip + "<h5/>");
     // Add marker to the chargeMarkers array
     chargeMarkers.push(chargeMarker);
   }
