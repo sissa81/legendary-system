cfa5153515add1e83509e8e3d5b34d796b0bbdf5
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-12 13:45:06 -0700

    Update Code

--- choxie/ev_population.js
+++ choxie/ev_population.js
@@ -1,70 +1,102 @@
 // Read in csv from git
-const csvdata = "https://raw.githubusercontent.com/sissa81/legendary-system/main/choxie/Data/yearly_data.csv";
+const csvdata = "https://raw.githubusercontent.com/sissa81/legendary-system/main/choxie/Data/ZIP.csv";
 
 // Fetch csv data and console log it
 // Convert # of vehicles to number instead of string
-d3.csv(csvdata).then(function(data) {
-  data.forEach(function(d){d['no_vehicles'] = +d['no_vehicles']; });
+var yearlyData = d3.csv(csvdata).then(function(data) {
+  data.forEach(function(d){d['Number of Vehicles'] = +d['Number of Vehicles']; });
   console.log(data);
 
-  x = [data[0]['reg_year'], data[1]['reg_year'], data[2]['reg_year'], data[3]['reg_year'], data[4]['reg_year'], 
-    data[5]['reg_year'], data[6]['reg_year'], data[7]['reg_year'], data[8]['reg_year'], data[9]['reg_year'], data[10]['reg_year']]
-  y = [data[0]['no_vehicles'], data[1]['no_vehicles'], data[2]['no_vehicles'], data[3]['no_vehicles'], data[4]['no_vehicles'], 
-    data[5]['no_vehicles'], data[6]['no_vehicles'], data[7]['no_vehicles'], data[8]['no_vehicles'], data[9]['no_vehicles'], data[10]['no_vehicles']]
 
-// Create Bar Chart
-  function barchart () {    
-    var bardata = [{
-        x: x,
-        y: y,
-        text: y.map(String),
-        textposition: 'auto',
-        
-        marker: {
-          color: ['#063734', '#004036', '#004834', '#00502f', '#005827', '#16652c', '#287331', '#398135', '#519a4e', '#6ab368', '#82cd82'],
-          line: {
-            color: '#146113',
-            width: 2
+
+// Nest and group by year.
+var groupedData = d3.nest()
+.key(function(d) {return d['Fuel Type']; })
+.key(function(d) {return d['Data Year']; })
+.rollup(function(v) {return d3.sum(v, function(d) {return d['Number of Vehicles']; }); })
+.entries(data);
+console.log(groupedData);
+// See how to pull out electric vehicle data
+console.log(groupedData[2]['values'][0]['key']);
+
+// Empty arrays to hold plot points
+xplots = []
+yplots = []
+
+// For loop to fill x plot point arrays
+for (let i = 0; i < groupedData[2]['values'].length; i++) {
+  let xplot = groupedData[2]['values'][i]['key']
+  xplots.push(xplot);
+};
+
+// Make sure x plot points are working
+console.log(xplots);
+
+// For loop to fill y plot point arrays
+for (let j = 0; j < groupedData[2]['values'].length; j++) {
+  let yplot = groupedData[2]['values'][j]['value']
+  yplots.push(yplot);
+};
+
+// Make sure y plot points are working
+console.log(yplots);
+
+// Create Line Chart
+function barchart () {    
+  var bardata = [{
+      x: xplots,
+      y: yplots,
+      text: yplots.map(String),
+      textposition: 'auto',
+      
+      marker: {
+        color: ['#063734', '#004036', '#004834', '#00502f', '#005827', '#16652c', '#287331', '#398135', '#519a4e', '#6ab368', '#82cd82'],
+        line: {
+          color: '#146113',
+          width: 2
+        }
+      },
+      type: 'bar'        
+  }];
+  
+  var barlayout = {                      
+      hovermode: 'closest',                
+      title: {
+        text: 'Historical Electric Vehicle Registraiton Data from California Energy Commission',
+        size: 18
+      },
+      xaxis: {
+        type: 'category',
+        title: {            
+          text: 'Year of Registration',
+          font: {
+            size: 18,
+            color: '#095907'
           }
         },
-        type: 'bar'        
-    }];
-    
-    var barlayout = {                      
-        hovermode: 'closest',                
+        tickangle: -45
+      },
+      yaxis: {
         title: {
-          text: 'Historical Electric Vehicle Registraiton Data from California Energy Commission',
-          size: 18
+          text: 'Number of Vehicles Registered',
+          font: {
+            size: 18,
+            color: '#095907'
+          }
         },
-        xaxis: {
-          type: 'category',
-          title: {            
-            text: 'Year of Registration',
-            font: {
-              size: 18,
-              color: '#095907'
-            }
-          },
-          tickangle: -45
-        },
-        yaxis: {
-          title: {
-            text: 'Number of Vehicles Registered',
-            font: {
-              size: 18,
-              color: '#095907'
-            }
-          },
-        },
-        height: 600,
-        width: 1000,        
-        showlegend: false
-    };
-    
-    var config = {responsive: true};
+      },
+      height: 600,
+      width: 1000,        
+      showlegend: false
+  };
+  
+  var config = {responsive: true};
 
-    Plotly.newPlot('bar', bardata, barlayout, config);
+  Plotly.newPlot('bar', bardata, barlayout, config);
 };  
 
 barchart();
+
 });
+
+
