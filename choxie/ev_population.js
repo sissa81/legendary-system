bceebdbe13943b2c74632c3125593dca629ff413
Charissa Hoxie <82292340+sissa81@users.noreply.github.com>
2021-09-11 19:00:43 -0700

    Update plotly

--- choxie/ev_population.js
+++ choxie/ev_population.js
@@ -12,25 +12,53 @@ d3.csv(csvdata).then(function(data) {
   y = [data[0]['no_vehicles'], data[1]['no_vehicles'], data[2]['no_vehicles'], data[3]['no_vehicles'], data[4]['no_vehicles'], 
     data[5]['no_vehicles'], data[6]['no_vehicles'], data[7]['no_vehicles'], data[8]['no_vehicles'], data[9]['no_vehicles'], data[10]['no_vehicles']]
 
-
 // Create Bar Chart
   function barchart () {    
     var bardata = [{
         x: x,
         y: y,
+        text: y.map(String),
+        textposition: 'auto',
+        
+        marker: {
+          color: ['#063734', '#004036', '#004834', '#00502f', '#005827', '#16652c', '#287331', '#398135', '#519a4e', '#6ab368', '#82cd82'],
+          line: {
+            color: '#146113',
+            width: 2
+          }
+        },
         type: 'bar'        
     }];
-  
-    var barlayout = {                
-        hovermode: 'closest',        
+    
+    var barlayout = {                      
+        hovermode: 'closest',                
+        xaxis: {
+          type: 'category',
+          title: {            
+            text: 'Year of Registration',
+            font: {
+              size: 18,
+              color: '#095907'
+            }
+          },
+          tickangle: -45
+        },
+        yaxis: {
+          title: {
+            text: 'Number of Vehicles Registered',
+            font: {
+              size: 18,
+              color: '#095907'
+            }
+          },
+        },
         height: 600,
-        width: 600,
+        width: 1000,        
         showlegend: false
     };
   
     Plotly.newPlot('bar', bardata, barlayout);
 };  
-
 
 barchart();
 });
